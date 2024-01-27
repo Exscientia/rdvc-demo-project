ea999572b4c8103747eaeaa841aeb428c4d96958
Dom Miketa <126485355+exs-dmiketa@users.noreply.github.com>
2024-01-27 20:15:50 +0000

    Feature/migrate to molflux (#2)
    * migrate to molflux
    
    * e2e train

--- src/rdvc_demo_project/stages/02_train.py
+++ src/rdvc_demo_project/stages/02_train.py
@@ -1,69 +1,41 @@
+from datasets import DatasetDict, Value, Sequence
 import dvc.api
-import lightning as L
-import numpy as np
-import pandas as pd
-import torch
-from dvclive import Live
-from torch import nn
-from torch.utils.data import DataLoader, TensorDataset
+from molflux import modelzoo as mz
+from molflux.datasets import load_dataset_from_store
 
 from rdvc_demo_project.utils import get_git_root
 
 
-def get_model(in_channels: int, hidden_channels: list[int]) -> torch.nn.Module:
-    layers: list[nn.Module] = [nn.Linear(in_channels, hidden_channels[0])]
-    last_channel_size = hidden_channels[0]
-
-    for out_channel_size in hidden_channels[1:]:
-        layers.append(nn.ReLU())
-        layers.append(nn.Linear(last_channel_size, out_channel_size))
-        last_channel_size = out_channel_size
-
-    layers.append(nn.ReLU())
-    layers.append(nn.Linear(last_channel_size, 1))
-
-    return nn.Sequential(*layers)
+def cast_x_features_to_float(dataset_dict: DatasetDict, x_feature_cols: str) -> DatasetDict:
+    for split in dataset_dict:
+        features = dataset_dict[split].features
+        for col in x_feature_cols:
+            features[col] = Sequence(Value("float"))
+        dataset_dict[split] = dataset_dict[split].cast(features)
+    return dataset_dict
 
 
 def main() -> None:
     config = dvc.api.params_show()
 
+    # Load dataset
     dataset_dir = get_git_root() / "data/featurised"
-    adme_data = pd.read_parquet(dataset_dir / "train.parquet", engine="pyarrow")
-    torch.tensor(np.array(adme_data["fingerprint"].to_list()))
-    torch.from_numpy(adme_data["Y"].to_numpy())
+    split_dataset = load_dataset_from_store(dataset_dir)
+    assert isinstance(split_dataset, DatasetDict)
+    split_dataset = cast_x_features_to_float(split_dataset, config["model"]["config"]["x_features"])
 
-    fingerprints = torch.tensor(np.array(adme_data["fingerprint"].to_list())).type("torch.FloatTensor")
-    solubilities = torch.from_numpy(adme_data["Y"].to_numpy()).type("torch.FloatTensor")
+    # DVCLive deposits training metrics into this directory
+    training_dir = get_git_root() / "metrics"
+    training_dir.mkdir(exist_ok=True)
 
-    dataset = TensorDataset(fingerprints, solubilities)
+    # Train model
+    model = mz.load_from_dict(config["model"])
+    model.train(train_data=split_dataset["train"])
 
-    # Featurise and save dataset splits
-    model = get_model(**config["model"])
-
-    fabric = L.Fabric(**config["fabric"])
-    fabric.launch()
-
-    dataloader = DataLoader(dataset, batch_size=config["batch_size"])
-    optimizer = torch.optim.AdamW(model.parameters(), **config["optimizer"])
-
-    model, optimizer = fabric.setup(model, optimizer)
-    dataloader = fabric.setup_dataloaders(dataloader)
-
-    model.train()
-
-    with Live(str(get_git_root() / "training")) as live:
-        for _ in range(config["num_epochs"]):
-            for batch in dataloader:
-                inputs, targets = batch
-                optimizer.zero_grad()
-                outputs = model(inputs)
-
-                loss = torch.nn.functional.mse_loss(outputs.squeeze(), targets.view(-1))
-                live.log_metric("train/loss", loss.item())
-                fabric.backward(loss)
-
-                optimizer.step()
+    # Save model
+    model_dir = get_git_root() / "model"
+    model_dir.mkdir(exist_ok=True)
+    mz.save_to_store(model_dir, model)
 
 
 if __name__ == "__main__":
