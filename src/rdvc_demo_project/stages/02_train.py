d44453892dd206bd0a79d1db2123c2e87fcc6d99
Dom Miketa <dmiketa@exscientia.co.uk>
2024-01-27 17:51:15 +0000

    e2e train

--- src/rdvc_demo_project/stages/02_train.py
+++ src/rdvc_demo_project/stages/02_train.py
@@ -1,9 +1,18 @@
-from datasets import DatasetDict
+from datasets import DatasetDict, Value, Sequence
 import dvc.api
 from molflux import modelzoo as mz
 from molflux.datasets import load_dataset_from_store
 
 from rdvc_demo_project.utils import get_git_root
+
+
+def cast_x_features_to_float(dataset_dict: DatasetDict, x_feature_cols: str) -> DatasetDict:
+    for split in dataset_dict:
+        features = dataset_dict[split].features
+        for col in x_feature_cols:
+            features[col] = Sequence(Value("float"))
+        dataset_dict[split] = dataset_dict[split].cast(features)
+    return dataset_dict
 
 
 def main() -> None:
@@ -13,6 +22,7 @@ def main() -> None:
     dataset_dir = get_git_root() / "data/featurised"
     split_dataset = load_dataset_from_store(dataset_dir)
     assert isinstance(split_dataset, DatasetDict)
+    split_dataset = cast_x_features_to_float(split_dataset, config["model"]["config"]["x_features"])
 
     # DVCLive deposits training metrics into this directory
     training_dir = get_git_root() / "metrics"
